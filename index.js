d553aea3d05d9094c7242de1fd19a88277ca313a
atistank <atistank95@gmail.com>
2017-10-06 20:16:44 +0700

    lan 2

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const User = require('./db.js')
+const models = require('./db.js')
 
 app.use(bodyParser.urlencoded({extended: true}))
 // cau hinh root
+
+
 app.get('/',(req,res)=> res.send('wellcome web cua tui'))
 
-app.post('/add',(req,res)=>{
-const user = req.body.u
-const pass = req.body.p
-User.create({
-     username: user,
-     password: pass
-    
-}).then(() => res.json({'ketqua': 1}))
-.catch(() => res.json({'ketqua': 0}))
-})
 
-app.post('/read',(req,res)=> {
-    User.findAll()
-    .then(users => res.json({'ketqua': 1, 'users': users}))
-    .catch(() => res.json({'ketqua': 0}))
+
+
+
+
+// read
+app.post('/user',(req,res)=> {
+    models.User.findAll()
+    .then(users => res.json({ketqua: 1, data: users}))
+    .catch(() => res.json({ketqua: 0}))
 
 })
-app.post('/update',(req,res)=>{
-        const {id, u, p} = req.body
-        User.update({
-                username: u,
-                password: p
 
-        },{
-                where: {id: id}
+// them user
+app.post('/add_user',(req,res)=> {
+let {userid,username,password,quyenhan,trangthai,like} = req.body
+models.User.create({
+     username,
+     password,
+     quyenhan,
+     trangthai,
+     like  
+})
+.then(() => res.json({ketqua: 1}))
+.catch(() => res.json({ketqua: 0}))
+})
 
-        }).then(row => res.json({'ketqua': 1, 'row': row[0]}))
-     .catch(() => res.json({'ketqua': 0}))
+
+
+
+// cap nhat user
+app.post('/update_user',(req,res)=>{
+let {userid,username,password,quyenhan,trangthai,like} = req.body
+   models.User.update({
+            username,
+            password,
+            quyenhan,
+            trangthai,
+            like
+        },{ where: {userid},returning: true })
+.then(row => res.json({ketqua: 1, rowsCount: row[0], data: row[1]  }))
+.catch(err => res.json({ketqua: 0, error: err.message} ))
 
 })//
+
+
+
+
+
+
 app.post('/delete',(req,res)=>{
-    const {id} = req.body
-    User.destroy({
-        where: {id: id}
+    let {id} = req.body
+     models.User.destroy({
+        where: {userid: id}
     })
+    .then( row => res.json( {ketqua: 1, rowsCount: row}) )
+    .catch(err => res.json( {ketqua: 0, error: err.message} ))
 
 })
 
 //tao port lang nghe cac ket noi
-app.listen(process.env.PORT || 3000, ()=>console.log('sever da khoi dong'))
 
-////
+const port = process.env.PORT || 3000
+app.listen(port, function(){ console.log(`server started on port ${port}`) })
